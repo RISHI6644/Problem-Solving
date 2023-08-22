752173e3d007a57a39f4e35f7148bf033aabf440
RISHI6644\RISHI <hrishikeshdas111@gmail.com>
2023-08-23 03:42:15 +0600

    Solved the first problem of Assignment 01 !

--- DSA-Bootcamp-Java/src/Assignment_01/LeapYear.java
+++ DSA-Bootcamp-Java/src/Assignment_01/LeapYear.java
@@ -1,2 +1,17 @@
-package Assignment_01;public class LeapYear {
+package Assignment_01;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if (year % 4 == 0) {
+            System.out.println("It is a Leap Year!!!");
+        }
+        else {
+            System.out.println("It is not a Leap Year!!!");
+        }
+    }
 }
